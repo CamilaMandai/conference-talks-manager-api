412b1870deb6e34155d696acf47f3d3c0499a19e
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-12-13 09:29:31 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
